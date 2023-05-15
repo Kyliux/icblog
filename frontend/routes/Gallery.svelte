dd53272c7bcc4826625449f933f993ed7e1c87ff
Kyliux <salocinigrub@gmail.com>
2023-05-15 22:46:33 +0200

    new looks almost good

--- frontend/routes/Gallery.svelte
+++ frontend/routes/Gallery.svelte
@@ -7,7 +7,7 @@
 
   import { Ed25519KeyIdentity } from "@dfinity/identity";
   import { getActor } from "./actor.js";
-  import { ofBlob } from "../src/CRC32";
+  import { updateChecksum }  from "../src/utils.js";
 
   let motoko_identity = Ed25519KeyIdentity.generate();
 
@@ -22,6 +22,7 @@
   let file_scaling_manager_actor;
   let file_storage_actor;
   let mediaFiles = [];
+  let checksum = 0;
 
   onMount(async () => {
     // Create actors using the agent and IDL factories
@@ -53,40 +54,45 @@
   });
 
   async function uploadFile(file) {
-    if (file_storage_actor) {
-      // Convert the file to a Blob
-      const blob = new Blob([file], { type: file.type });
+  if (file_storage_actor) {
+    // Convert the file to a Blob
+    const blob = new Blob([file], { type: file.type });
 
-      // Create a new chunk in the file storage canister
-      const chunkIdResult = await file_storage_actor.uploadChunk("", blob, 0);
-      if (chunkIdResult.ok) {
-        const chunkId = chunkIdResult.ok;
+    var batch_id = Math.random().toString(36).substring(2, 7);
 
-        // Commit the batch and create the asset in the file storage canister
-        const commitResult = await file_storage_actor.commit_batch("", [chunkId], {
-          checksum: 0, // Provide the correct checksum
-          content_encoding: "",
-          content_type: file.type,
-          filename: file.name
-        });
-        if (commitResult.ok) {
-          // Asset creation successful, refresh the media files list
-          const updatedResult = await file_storage_actor.assets_list();
-          if (updatedResult.ok) {
-            mediaFiles = updatedResult.ok;
-          } else {
-            console.error("Error fetching media files:", updatedResult.err);
-          }
-        } else {
-          console.error("Error committing batch and creating asset:", commitResult.err);
-        }
-      } else {
-        console.error("Error creating chunk:", chunkIdResult.err);
+    const uploadChunk = async ({ chunk, order }) => {
+      console.error("return a chunk");
+
+      return file_storage_actor.create_chunk(batch_id, chunk, order);
+    };
+
+    const asset_reader = new FileReader(file);
+
+    asset_reader.onloadend = async () => {
+      const asset_unit8Array = new Uint8Array(asset_reader.result);
+      console.error("return a chunk", asset_unit8Array);
+      const promises = [];
+      const chunkSize = 2000000;
+      let checksum = 0;
+
+      for (let start = 0, index = 0; start < asset_unit8Array.length; start += chunkSize, index++) {
+        const chunk = asset_unit8Array.slice(start, start + chunkSize);
+
+       checksum = updateChecksum(chunk, checksum);
+
+        promises.push(uploadChunk({ chunk, order: index }));
+        console.error("return a chunk", asset_unit8Array);
+
       }
-    } else {
-      console.error("Error uploading file: File storage actor not available");
-    }
+
+      const chunk_ids = await Promise.all(promises);
+
+      // Perform further operations with chunk_ids or handle the upload completion
+    };
+
+    asset_reader.readAsArrayBuffer(file);
   }
+}
 
 
 </script>
